367391857254cc982cb47784039990215f97a02c
euna <christinejung10@daum.net>
2023-02-13 20:31:09 +0900

    feat(chapter02/Splitter): 커스텀 구분자 기준 문자 분리 기능 구현

--- src/main/java/chapter02/Splitter.java
+++ src/main/java/chapter02/Splitter.java
@@ -1,13 +1,19 @@
 package chapter02;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Splitter {
 
-    public static List<String> separate(String input) {
-        List<String> numbers = Arrays.asList(input.split(",|:"));
-        return numbers;
+    final String defaultSeparatorForm_regex = ",|:";
+    final String customSeparatorForm_regex = "//(.)\n(.*)";
+
+    public String[] splitInput(String input) {
+        Matcher matcher = Pattern.compile(customSeparatorForm_regex).matcher(input);
+        if (matcher.find()) {
+            return matcher.group(2).split(matcher.group(1));
+        }
+        return input.split(defaultSeparatorForm_regex);
     }
 
 }
--- src/test/java/chapter02/SplitterTest.java
+++ src/test/java/chapter02/SplitterTest.java
@@ -1,22 +1,35 @@
 package chapter02;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
 
 public class SplitterTest {
 
+    Splitter splitter;
+    String input;
+
+    @BeforeEach
+    void setup() {
+        splitter = new Splitter();
+    }
+
     @Test
     void 문자분리_기본구분자_콤마() {
-        String input = "1,2";
-        Assertions.assertThat(Splitter.separate(input)).isEqualTo(Arrays.asList("1", "2"));
+        input = "1,2";
+        Assertions.assertThat(splitter.splitInput(input)).isEqualTo(new String[]{"1", "2"});
     }
 
     @Test
     void 문자분리_기본구분자_콜론() {
-        String input = "1:2";
-        Assertions.assertThat(Splitter.separate(input)).isEqualTo(Arrays.asList("1", "2"));
+        input = "1:2";
+        Assertions.assertThat(splitter.splitInput(input)).isEqualTo(new String[]{"1", "2"});
+    }
+
+    @Test
+    void 문자_분리_커스텀_구분자() {
+        input = "//;\n1;2";
+        Assertions.assertThat(splitter.splitInput(input)).isEqualTo(new String[]{"1", "2"});
     }
 
 }
